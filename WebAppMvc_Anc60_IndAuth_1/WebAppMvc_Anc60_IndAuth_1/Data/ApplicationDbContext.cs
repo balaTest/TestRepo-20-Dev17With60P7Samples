886d7d250988351b10cad180488ed7b8bac522c0
Bala Chirtsabesan <balaTest@live.com>
2021-07-20 09:46:32 -0700

    [WebAppMvc_Anc60_IndAuth_1] Created solution

---
+++ WebAppMvc_Anc60_IndAuth_1/WebAppMvc_Anc60_IndAuth_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppMvc_Anc60_IndAuth_1.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ WebAppMvc_Anc60_IndAuth_1/WebAppMvc_Anc60_IndAuth_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+
+namespace WebAppMvc_Anc60_IndAuth_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
